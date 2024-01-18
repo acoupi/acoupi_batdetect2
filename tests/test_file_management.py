5bccda1b8238c69ef428228aa29da310202b1704
audevuilli <aude.vuilli@gmail.com>
2024-01-18 17:03:03 +0000

    modify tests file management

--- tests/test_file_management.py
+++ tests/test_file_management.py
@@ -6,7 +6,7 @@ import os
 
 from acoupi import components, data
 
-# from acoupi.files import TEMP_PATH
+from acoupi.files import TEMP_PATH
 from acoupi.system.configs import CeleryConfig
 from celery import Celery
 from celery.worker import WorkController
@@ -19,7 +19,7 @@ from acoupi_batdetect2.configuration import (
 )
 from acoupi_batdetect2.program import BatDetect2_Program
 
-TEMP_PATH = Path("Users/audevuilliomenet/Documents")
+#TEMP_PATH = Path("Users/audevuilliomenet/Documents")
 
 
 @pytest.fixture
@@ -142,13 +142,12 @@ def test_management_tempfiles_notprocess(
     )
 
     # Retrieve the recording from the database.
-    recordings, model_outputs = program.store.get_recordings(
-        ids=[temp_recording.id],
+    recordings = program.store.get_recordings_temp_path(
+        temp_paths=[str(temp_recording.path)],
     )
 
     # Check that the recording was stored in the database but no detection exists.
     assert len(recordings) >= 1
-    assert len(model_outputs) == 0
 
     assert "file_management_task" in program.tasks
     output_file_task = program.tasks["file_management_task"].delay()
@@ -205,63 +204,3 @@ def test_management_tempfile_positive_detection(
 
 def test_management_tempfile_negative_detection():
     return
-
-
-def test_can_run_management_program(
-    tmp_path: Path,
-    recording: data.Recording,
-    celery_app: Celery,
-    celery_worker: WorkController,
-):
-    """Test can run detection program."""
-    config = BatDetect2_ConfigSchema(
-        dbpath=tmp_path / "test.db",
-        dbpath_messages=tmp_path / "test_messages.db",
-        audio_directories=AudioDirectories(
-            audio_dir=tmp_path / "audio",
-            audio_dir_true=tmp_path / "audio_true",
-            audio_dir_false=tmp_path / "audio_false",
-        ),
-    )
-    store = components.SqliteStore(config.dbpath)
-
-    # Get the test recording in the database .
-    store.store_recording(recording)
-
-    # Instantiate the BatDetect2 program.
-    celery_config = CeleryConfig()
-    program = BatDetect2_Program(
-        program_config=config,
-        celery_config=celery_config,
-        app=celery_app,
-    )
-    program.logger.setLevel("DEBUG")
-    assert celery_app.conf["accept_content"] == ["pickle"]
-    celery_worker.reload()
-
-    # Check that the program has a detection task.
-    assert "detection_task" in program.tasks
-
-    # Run the detection task on the test recording.
-    model_output = program.tasks["detection_task"].delay(recording)
-    model_output.get()
-
-    # Retrieve the recording from the database.
-    recordings, model_outputs = store.get_recordings(
-        ids=[recording.id],
-    )
-
-    # Check that the recording was stored in the database.
-    assert len(recordings) == 1
-    assert len(model_outputs) == 1
-    assert recordings[0] == recording
-
-    # Check that the recording files with positive detection was moved.
-    recording_file = [0][0]
-    assert isinstance(detections, data.ModelOutput)
-    assert len(detections.detections) > 0
-
-    # Check that the recording files with negative detection was moved.
-    detections = model_outputs[0][0]
-    assert isinstance(detections, data.ModelOutput)
-    assert len(detections.detections) > 0
