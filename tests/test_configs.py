d328f997e06f7365e44ae76aaffccee88e3fe3e2
mbsantiago <santiago.mbal@gmail.com>
2024-10-11 15:34:23 +0100

    Added a simple check that the config has been overwriten

---
+++ tests/test_configs.py
@@ -0,0 +1,17 @@
+import datetime
+
+from acoupi_batdetect2.configuration import (
+    BatDetect2_ConfigSchema,
+)
+
+
+def test_audio_config_defaults_have_been_overwriten(
+    microphone_config, messaging_config
+):
+    config = BatDetect2_ConfigSchema(
+        microphone=microphone_config,
+        messaging=messaging_config,
+    )
+
+    assert config.recording.schedule_start == datetime.time(hour=18)
+    assert config.recording.schedule_end == datetime.time(hour=6)
