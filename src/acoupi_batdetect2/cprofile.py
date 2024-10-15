f07995e244c4defb0b3bb7ff389c9bd2381e1d0f
audevuilli <aude.vuilli@gmail.com>
2024-10-15 18:06:54 +0100

    update cprofile

--- src/acoupi_batdetect2/cprofile.py
+++ src/acoupi_batdetect2/cprofile.py
@@ -1,6 +1,7 @@
 import datetime
 
-from acoupi import components
+import pytz
+from acoupi import components, data
 from acoupi.components import types
 from acoupi.programs.templates import (
     AudioConfiguration,
@@ -27,6 +28,17 @@ class AudioConfig(AudioConfiguration):
     )
 
 
+class SaveRecordingFilter(BaseModel):
+    """Recording saving options configuration."""
+
+    starttime: datetime.time = datetime.time(hour=8, minute=0, second=0)
+    endtime: datetime.time = datetime.time(hour=20, minute=0, second=0)
+    before_dawndusk_duration: int = 0
+    after_dawndusk_duration: int = 0
+    frequency_duration: int = 0
+    frequency_interval: int = 0
+
+
 class SaveRecordingManager(BaseModel):
     """Configruation options to manage saving recordings."""
 
@@ -50,6 +62,9 @@ class BatDetect2_ConfigSchema(cProfileProgram_Configuration):
     model: ModelConfig = Field(
         default_factory=ModelConfig,
     )
+    saving_filters: SaveRecordingFilter = Field(
+        default_factory=SaveRecordingFilter,
+    )
     saving_managers: SaveRecordingManager = Field(
         default_factory=SaveRecordingManager,
     )
@@ -71,6 +86,68 @@ class BatDetect2_Program(cProfileProgram[BatDetect2_ConfigSchema]):
                 detection_threshold=config.model.detection_threshold
             )
         ]
+
+    def get_recording_saving_filters(self, config) -> list[types.RecordingSavingFilter]:
+        if not config.saving_filters:
+            # No saving filters defined
+            return []
+
+        saving_filters = []
+        timezone = pytz.timezone(config.timezone)
+        recording_saving_filters = config.saving_filters
+
+        # Main filter
+        # Will only save recordings if the recording time is in the
+        # interval defined by the start and end time.
+        if (
+            recording_saving_filters.starttime is not None
+            and recording_saving_filters.endtime is not None
+        ):
+            saving_filters.append(
+                components.SaveIfInInterval(
+                    interval=data.TimeInterval(
+                        start=recording_saving_filters.starttime,
+                        end=recording_saving_filters.endtime,
+                    ),
+                    timezone=timezone,
+                )
+            )
+
+        # Additional filters
+        if (
+            recording_saving_filters.frequency_duration != 0
+            and recording_saving_filters.frequency_interval != 0
+        ):
+            # This filter will only save recordings at a frequency defined
+            # by the duration and interval.
+            saving_filters.append(
+                components.FrequencySchedule(
+                    duration=recording_saving_filters.frequency_duration,
+                    frequency=recording_saving_filters.frequency_interval,
+                )
+            )
+
+        if recording_saving_filters.before_dawndusk_duration != 0:
+            # This filter will only save recordings if the recording time
+            # is before dawn or dusk.
+            saving_filters.append(
+                components.Before_DawnDuskTimeInterval(
+                    duration=recording_saving_filters.before_dawndusk_duration,
+                    timezone=timezone,
+                )
+            )
+
+        if recording_saving_filters.after_dawndusk_duration != 0:
+            # This filter will only save recordings if the recording time
+            # is after dawn or dusk.
+            saving_filters.append(
+                components.After_DawnDuskTimeInterval(
+                    duration=recording_saving_filters.after_dawndusk_duration,
+                    timezone=timezone,
+                )
+            )
+
+        return saving_filters
 
     def get_recording_saving_managers(self, config):
         if not config.saving_managers:
