45965d3877c46dbeca6c6edb9c34e1063d3930c3
mbsantiago <santiago.mbal@gmail.com>
2024-11-29 14:01:16 +0000

    Added the ThresholdFilter to saving_filters

--- src/acoupi_batdetect2/program.py
+++ src/acoupi_batdetect2/program.py
@@ -1,63 +1,63 @@
 """# Batdetect2 Program.
 
 This module builds the BatDetect2 Program to record, detect and classify, as well as
-manage and send messages of UK bat calls. The program extends the `DetectionProgram` 
-and `MessagingProgram` from the _acoupi_ package by adding the BatDetect2 model 
+manage and send messages of UK bat calls. The program extends the `DetectionProgram`
+and `MessagingProgram` from the _acoupi_ package by adding the BatDetect2 model
 and integrating users' custom configuration schema.
 
 ### Key Elements:
 
-- __BatDetect2_ConfigSchema__: Defines the configuration for the BatDetect2 
-program, including the audio recording, model setup, file management, messaging, 
+- __BatDetect2_ConfigSchema__: Defines the configuration for the BatDetect2
+program, including the audio recording, model setup, file management, messaging,
 and summariser settings.
 
-### Program Tasks: 
+### Program Tasks:
 
 - __recording_task__: Records audio from a microphone and saves the audio files
-in a temporary directory until they have been processed by the `detection` 
-and `management` tasks. Based on the `SavingFilters` configuration, recordings 
+in a temporary directory until they have been processed by the `detection`
+and `management` tasks. Based on the `SavingFilters` configuration, recordings
 will either saved or deleted.
 - __detection_task__: Runs the BatDetect2 model on the audio recordings, processes
 the detections, and can use a custom `ModelOutputCleaner` to filter out unwanted
 detections (e.g., low-confidence results). The filtered detections are saved in
-a `metadata.db` file. 
-- __management_task__: Performs periodically file management operations, 
+a `metadata.db` file.
+- __management_task__: Performs periodically file management operations,
 such as moving recording to permanent storage, or deleting unnecessary ones.
-- __messaging_task__: Send messages stored in the message store using a 
-configured protocol (HTTP or MQTT). 
-- __summary_task__: Periodically creates summaries of the detections. 
+- __messaging_task__: Send messages stored in the message store using a
+configured protocol (HTTP or MQTT).
+- __summary_task__: Periodically creates summaries of the detections.
 
 ### Customisation Options:
 
-- __ModelConfig__: Set the `detection_threshold` to clean out the output of the 
-BatDetect2 model. Detections with a confidence score below this threshold 
+- __ModelConfig__: Set the `detection_threshold` to clean out the output of the
+BatDetect2 model. Detections with a confidence score below this threshold
 will be excluded from the store and from the message content.
 
-- __SaveRecordingManager__: Define where recordings are stored, the naming format, and 
-the minimum confidence score for saving recordings. Recordings with confidence 
-scores below the `saving_threshold` will not be saved. The `saving_threshold` 
-can be set lower than the `detection_threshold` to save recordings with uncertain 
-detections. Recordings with detections above the `detection_threshold` will be 
-saved in the `true_dir` directory, while recordings with detections below 
-the `detection_threshold` but above the `saving_threshold` will be saved in 
-the `false_dir` directory. 
+- __SaveRecordingManager__: Define where recordings are stored, the naming format, and
+the minimum confidence score for saving recordings. Recordings with confidence
+scores below the `saving_threshold` will not be saved. The `saving_threshold`
+can be set lower than the `detection_threshold` to save recordings with uncertain
+detections. Recordings with detections above the `detection_threshold` will be
+saved in the `true_dir` directory, while recordings with detections below
+the `detection_threshold` but above the `saving_threshold` will be saved in
+the `false_dir` directory.
 
-- __SaveRecordingFilter__: Define additional saving filters for saving recordings. 
+- __SaveRecordingFilter__: Define additional saving filters for saving recordings.
     1. A timeinterval interval fitler that saves recordings whthin a specific time
-     window, set by the `starttime` and `endtime` parameters. 
+     window, set by the `starttime` and `endtime` parameters.
     2. A frequency filter that saves recordings for a specific duration
-     (in minutes) at defined interval (in minutes), set by the `frequency_duration` 
+     (in minutes) at defined interval (in minutes), set by the `frequency_duration`
      and `frequency_interval` parameters.
     3. A before dawn/dusk filter to save recording for a defined duration
      (in minutes) before dawn and dusk, set by the `before_dawndusk_duration`.
-    4. An after dawn/dusk filter to save recording for a defined duration 
+    4. An after dawn/dusk filter to save recording for a defined duration
      (in minutes) after dawn and dusk, set by the `after_dawndusk_duration`.
 
-- __SummariserConfig__: Define the interval for summarising detections. 
-By default, the summariser calculates the minimum, maximum, and average 
-confidence scores of the total number of detections for each time interval. 
-If the `low_band_threshold`, `mid_band_threshold`, and `high_band_threshold` are 
-set to values greater than 0.0, it also summarises the number of detections in 
+- __SummariserConfig__: Define the interval for summarising detections.
+By default, the summariser calculates the minimum, maximum, and average
+confidence scores of the total number of detections for each time interval.
+If the `low_band_threshold`, `mid_band_threshold`, and `high_band_threshold` are
+set to values greater than 0.0, it also summarises the number of detections in
 each band (low, mid, high).
 """
 
@@ -99,7 +99,9 @@ class BatDetect2_Program(DetectionProgram[BatDetect2_ConfigSchema]):
 
             self.add_task(
                 function=summary_task,
-                schedule=datetime.timedelta(minutes=config.summariser_config.interval),
+                schedule=datetime.timedelta(
+                    minutes=config.summariser_config.interval
+                ),
             )
 
     def configure_model(self, config):
@@ -179,12 +181,13 @@ class BatDetect2_Program(DetectionProgram[BatDetect2_ConfigSchema]):
         return [
             components.SaveRecordingManager(
                 dirpath=config.paths.recordings,
-                dirpath_true=config.paths.recordings / config.saving_managers.true_dir,
+                dirpath_true=config.paths.recordings
+                / config.saving_managers.true_dir,
                 dirpath_false=config.paths.recordings
                 / config.saving_managers.false_dir,
                 timeformat=config.saving_managers.timeformat,
                 detection_threshold=config.model.detection_threshold,
-                saving_threshold=config.saving_managers.saving_threshold,
+                saving_threshold=config.saving_managers.bat_threshold,
             )
         ]
 
@@ -210,7 +213,9 @@ class BatDetect2_Program(DetectionProgram[BatDetect2_ConfigSchema]):
             )
         ]
 
-    def get_recording_filters(self, config) -> list[types.RecordingSavingFilter]:
+    def get_recording_filters(
+        self, config
+    ) -> list[types.RecordingSavingFilter]:
         """Get the recording filters for the BatDetect2 Program.
 
         Parameters
@@ -290,6 +295,13 @@ class BatDetect2_Program(DetectionProgram[BatDetect2_ConfigSchema]):
                     duration=recording_saving.after_dawndusk_duration,
                     timezone=timezone,
                 )
+            )
+
+        if recording_saving is not None and recording_saving.saving_threshold:
+            # This filter will only save recordings if the recording has
+            # detections above the saving threshold.
+            saving_filters.append(
+                components.SavingThreshold(recording_saving.saving_threshold)
             )
 
         return saving_filters
